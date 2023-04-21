8d4bc6b16fa39f33bb69dea3112e2681786abdc6
Abhinav Saini <abhinavsaini124@gmail.com>
2023-04-21 16:32:24 +0530

    Init 1

---
+++ script.js
@@ -0,0 +1,20 @@
+const navcont = document.querySelector('.navbar-container');
+const navbar = document.querySelector('.nav-bar');
+const navlinks = document.querySelector('.nav-links');
+const btn = document.getElementById('nav-btn');
+const icon = document.querySelector('.fas.fa-bars');
+
+var typed = new Typed('.type', {
+    strings: ['Data Scientist.', 'Researcher.', 'Designer.', 'Pythoneer.', 'Content Creator.', 'Student.'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+})
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    navlinks.classList.toggle("expand");
+    navcont.classList.toggle("expand");
+    navbar.classList.toggle("expand");
+    icon.classList.toggle("fa-times");
+});
